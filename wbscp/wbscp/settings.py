21645e9c8d3e4b9f74d4ed95adc8ff0b0ada1a24
paulotozi <paulotozi1512@gmail.com>
2023-04-11 23:29:25 -0300

    adicionando o arquivo index.html

--- wbscp/wbscp/settings.py
+++ wbscp/wbscp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -19,8 +20,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'wbscp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR), 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
